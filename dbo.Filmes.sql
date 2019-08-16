c93d707657df5dd901432492d1079e2c90384b00
lucasmoroski <33026228+lucasmoroski@users.noreply.github.com>
2019-08-16 02:13:04 -0300

    Add files via upload

---
+++ dbo.Filmes.sql
@@ -0,0 +1,9 @@
+create table DboFilme(
+	Id_film int identity(1,1) primary key,
+	Titulof varchar(200),
+	Datalanc varchar(200),
+	Categoriafilme varchar(200),
+	Nomediretor varchar(200),
+	ListaAtores varchar(200)	
+)
+
